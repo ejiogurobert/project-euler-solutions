38e963f6431e8ba5bbad7b14e3139ac8ac02b38b
Ndubuisi Ejiogu <ndubuisi.ejiogu@thebulbafrica.institute>
2022-02-25 18:18:21 +0100

    Completed archive problem 5

--- archive5.js
+++ archive5.js
@@ -1,10 +1,63 @@
-function isEvenlyMultiple(num) {
-  let range = [2, 4, 6, 7, 8, 9, 10];
-  for (let i = 0; i <= range.length; i++) {
-    if (num % range[i] === 0) {
-      return true;
+let evenRange = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+
+// function isEvenlyMultiple(num) {
+//   for (let i = 0; i <= evenRange.length; i++) {
+//     if (num % evenRange[i] === 0) {
+//       return true;
+//     }
+//   }
+// }
+
+function smallestMultiple(n) {
+  // Get the smallest common multiple by common division method
+  for (let i = 0; i <= evenRange.length; i++) {
+    let prime = 2;
+    let number = evenRange[i];
+    if (number % prime === 0) {
+      let lowest;
+      number /= prime;
+    } else {
+      prime++;
     }
   }
+  // prime = prime * prime;
+  // lowest = prime;
+  // console.log(lowest);
+}
+smallestMultiple();
+function smallestMult(n) {
+  // Boolean that checks the divisibility status
+  let isDivisible = false;
+  let currentMultiple;
+  const array = generateArray(n);
+  // checks all the value from n to one and reassigns divisibility if all can divide a  multiple of n
+
+  let i = 2;
+
+  while (isDivisible === false) {
+    currentMultiple = n * i;
+
+    const newArray = array.filter((item) => currentMultiple % item === 0);
+
+    if (newArray.length === array.length) {
+      isDivisible = true;
+    }
+
+    i++;
+  }
+
+  return currentMultiple;
 }
 
-function smallestMultiple(multiples) {}
+function generateArray(n) {
+  const array = [];
+
+  for (let i = 1; i <= n; i++) {
+    array.push(i);
+  }
+
+  return array;
+}
+
+let curr = smallestMult(20);
+console.log(curr);
